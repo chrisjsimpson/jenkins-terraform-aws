59d5535bfa4813547882c9ac9cd180e77e56a1ec
Simpson, Chris <chris.simpson@capgemini.com>
2019-11-14 14:41:13 +0000

    added jenkins user_data script to install java 1.8, and then jenkins, and start the jenkins service

--- main.tf
+++ main.tf
@@ -109,6 +109,18 @@ resource "aws_instance" "jenkins" {
   associate_public_ip_address = true  #TODO dont expose like this
   source_dest_check           = false #To allow NAT or VPN access
 
+  user_data = <<EOF
+          #! /bin/bash
+          sudo yum update -y
+          sudo yum remove java -y
+          sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+          sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+          sudo yum install java-1.8.0-openjdk -y
+          sudo yum install jenkins -y
+          sudo service jenkins start
+
+  EOF
+
   tags = {
     Name = "Jenkins"
   }
